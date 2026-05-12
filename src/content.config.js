eb35a69795423c06ba7a987119f9e9021267d364
Junya Kobayashi <168046274+junya005@users.noreply.github.com>
2026-05-12 16:11:08 +0900

    content.config.js を作成

---
+++ src/content.config.js
@@ -0,0 +1,17 @@
+import { defineCollection, z } from 'astro:content';
+import { file } from 'astro/loaders';
+
+const works = defineCollection({
+  loader: file("src/data/projects.json"),
+  schema: z.object({
+    id: z.string(),
+    category: z.string(),
+    title: z.string(),
+    description: z.string(),
+    thumbnailPath: z.string(),
+    detailParh: z.string(),
+    projectPath: z.string()
+  })
+});
+
+export const collections = { works };
